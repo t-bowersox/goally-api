4dcb01c3b5a2324041dec9d6836f838aacf0b7aa
Tim Bowersox <11789189+t-bowersox@users.noreply.github.com>
2023-07-24 20:25:37 -0400

    Feature: Logout Fixes #5

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { verifyPassword } from "../lib/crypto.js";
 import { unauthorized } from "../lib/responses.js";
 import { getUserByEmail } from "../lib/users.js";
+import { AuthenticationMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -25,6 +26,11 @@ router.post("/login", async (request, response) => {
   }
 
   request.session = { userId: user.id };
+  return response.json(true);
+});
+
+router.get("/logout", AuthenticationMiddleware, (request, response) => {
+  request.session = null;
   return response.json(true);
 });
 
