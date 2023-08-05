ec4bf96b257c0ec72f1de4c718548cf574b58bc0
Tim Bowersox <11789189+t-bowersox@users.noreply.github.com>
2023-08-05 12:06:13 -0400

    Implement graceful shutdown & health checks Fixes #22

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+import { createTerminus } from "@godaddy/terminus";
 import "dotenv/config";
+import { createServer } from "node:http";
 import { app } from "./app.js";
+import { healthCheck, onSignal } from "./lib/terminus.js";
 
 const port = Number.parseInt(process.env.API_PORT ?? "3000");
+const server = createServer(app);
 
-app.listen(port, () => {
+createTerminus(server, {
+  healthChecks: { "/health-check": healthCheck },
+  onSignal,
+});
+
+server.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
